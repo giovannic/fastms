3e338d56caa659cdaa7d90c108d842594179373a
Giovanni <giovanni.charles@gmail.com>
2021-08-18 14:50:21 +0000

    Distribute for multiple GPUs

--- src/fastms/model.py
+++ src/fastms/model.py
@@ -1,13 +1,20 @@
+from tensorflow.config import list_physical_devices
+from tensorflow.distribute import MirroredStrategy, get_strategy
 from tensorflow.random import set_seed
 from tensorflow import keras, make_ndarray
 from tensorflow.keras import layers
 from tensorflow.keras.wrappers.scikit_learn import KerasRegressor
 
 def create_model(optimiser, n_layer, dropout, loss, **kwargs):
-    model = keras.Sequential()
-    model.add(layers.LSTM(n_layer[0], dropout=dropout, return_sequences=True))
-    model.add(layers.LSTM(n_layer[1], dropout=dropout, return_sequences=True))
-    model.add(layers.TimeDistributed(layers.Dense(n_layer[1])))
+    if list_physical_devices('GPU'):
+      strategy = MirroredStrategy()
+    else:  # Use the Default Strategy
+      strategy = get_strategy()
+    with strategy.scope():
+        model = keras.Sequential()
+        model.add(layers.LSTM(n_layer[0], dropout=dropout, return_sequences=True))
+        model.add(layers.LSTM(n_layer[1], dropout=dropout, return_sequences=True))
+        model.add(layers.TimeDistributed(layers.Dense(n_layer[1])))
     model.compile(loss=loss, optimizer=optimiser, metrics=['mean_squared_error'])
     return model
 
