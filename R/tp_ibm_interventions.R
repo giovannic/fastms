a4de61e07d91b2a9147faf97da80257409ecb533
Giovanni Charles <giovanni.charles@gmail.com>
2021-08-25 12:23:33 +0100

    Update intervention script:
     * Add fitting/sensitivity params
     * Unrestrict intervention ranges

--- R/tp_ibm_interventions.R
+++ R/tp_ibm_interventions.R
@@ -17,14 +17,73 @@ month <- 30
 fits <- readRDS(global_fits)
 
 set.seed(seed)
+
+db <- 10
+dc <- 30
+a0 <- 2920
+rho<- 0.85
+immunity_scale <- function(a, d) {
+  (1 + rho / (d / a0 - 1) * exp(-a /a0) - (1 + rho/(d / a0 + 1)) * exp(-a / d))
+}
+
 n <- n_batches * batch_size
 seasonality <- do.call(rbind, fits$seasonality)
 seasonality <- unique(seasonality)
 params <- data.frame(
   average_age = runif(n, 20 * 365, 40 * 365),
-  init_EIR = runif(n, 0, 100),
-  Q0 = runif(n, 0, 1)
+  init_EIR = runif(n, 0, 1000),
+  sigma_squared = runif(n, 1, 3),
+  du = runif(n, 30, 100),
+  kb = runif(n, 0.01, 10),
+  ub = runif(n, 1, 1000),
+  uc = runif(n, 1, 1000), # not documented
+  ud = runif(n, 1, 1000), # not documented
+  kc = runif(n, 0.01, 10),
+  b0 = runif(n, 0.01, 0.99),
+  ct = runif(n, 0, 1),
+  cd = runif(n, 0, 1),
+  #ca = runif(n, 0, 1), #TODO: how is this changed?
+  cu = runif(n, 0, 1)
 )
+
+daily_EIR <- params$init_EIR / 365
+
+params$ib0 <- vapply(
+  runif(n, 1, 30) * 365,
+  function(a) daily_EIR * db / (daily_EIR * params$ub + 1) * immunity_scale(a, db),
+  numeric(1)
+)
+params$ic0 <- vapply(
+  runif(n, 1, 30) * 365,
+  function(a) daily_EIR * dc * immunity_scale(a, dc),
+  numeric(1)
+)
+
+params$b1 <- runif(n, 0, 1) * params$b0
+
+mosquito_params <- lapply(
+  seq(n),
+  function(.) {
+    lapply(
+      c('gamb', 'arab', 'fun'),
+      function(label) {
+        list(
+          species = label,
+          mum = 1 / runif(n, 3, 20),
+          blood_meal_rates = 1 / runif(n, 1, 4),
+          #forage = runif(n, 0, 1), #TODO: not parameterisable yet
+          Q0 = runif(n, 0, 1),
+          phi_indoors = runif(n, 0, 1),
+          phi_bednets = runif(n, 0, 1)
+        )
+      }
+    )
+  }
+)
+
+mosquito_proportions <- matrix(runif(n * 3), nrow = n, ncol = 3)
+mosquito_proportions <- mosquito_proportions / rowSums(mosquito_proportions)
+
 params <- cbind(
   params,
   seasonality[sample(nrow(seasonality), n, replace = TRUE), ]
@@ -65,7 +124,7 @@ irs <- lapply(
   irs,
   function(coverages) {
     active <- runif(future_years, 0, 1) > .5
-    c(coverages, active * runif(future_years, 0.8, 0.85))
+    c(coverages, active * runif(future_years, 0., 0.85))
   }
 )
 
@@ -78,7 +137,7 @@ smc <- lapply(
   smc,
   function(coverages) {
     active <- runif(future_years, 0, 1) > .5
-    c(coverages, active * runif(future_years, 0.8, 0.85))
+    c(coverages, active * runif(future_years, 0., 0.85))
   }
 )
 
@@ -87,7 +146,7 @@ rtss <- lapply(
   seq(n),
   function(.) {
     active <- runif(future_years, 0, 1) > .5
-    active * runif(future_years, 0.8, 0.85)
+    active * runif(future_years, 0., 0.85)
   }
 )
 
@@ -121,13 +180,33 @@ process_row <- function(i) {
     g0 = row$seasonal_a0,
     g = c(row$seasonal_a1, row$seasonal_a2, row$seasonal_a3),
     h = c(row$seasonal_b1, row$seasonal_b2, row$seasonal_b3),
-    Q0 = row$Q0,
     average_age = row$average_age,
+    sigma_squared = row$sigma_squared,
+    du = row$du,
+    kb = row$kb,
+    ub = row$ub,
+    uc = row$uc,
+    ud = row$ud,
+    kc = row$kc,
+    b0 = row$b0,
+    b1 = row$b1,
+    ib0 = row$ib0,
+    ic0 = row$ic0,
+    ct = row$ct,
+    cd = row$cd,
+    #ca = row$ca, #TODO: as above
+    cu = row$cu,
     prevalence_rendering_min_ages = 0,
     prevalence_rendering_max_ages = 100 * year,
     clinical_incidence_rendering_min_ages = 0,
     clinical_incidence_rendering_max_ages = 100 * year
   ))
+
+  parameters <- malariasimulation::set_species(
+    parameters,
+    mosquito_params[[i]],
+    proportions = mosquito_proportions[i,]
+  )
 
   parameters <- malariasimulation::set_drugs(
     parameters,
@@ -148,9 +227,9 @@ process_row <- function(i) {
     timesteps = seq(0, period - 1) * year + (warmup * year),
     coverages = llins[[i]],
     retention = 5 * year,
-    dn0 = matrix(.533, nrow=period, ncol=1),
-    rn = matrix(.56, nrow=period, ncol=1),
-    rnm = matrix(.24, nrow=period, ncol=1),
+    dn0 = matrix(.533, nrow=period, ncol=3),
+    rn = matrix(.56, nrow=period, ncol=3),
+    rnm = matrix(.24, nrow=period, ncol=3),
     gamman = rep(2.64 * year, period)
   )
 
@@ -159,12 +238,12 @@ process_row <- function(i) {
 		parameters,
     timesteps = seq(0, period - 1) * year + (warmup * year),
 		coverages = irs[[i]],
-		ls_theta = matrix(2.025, nrow=period, ncol=1),
-		ls_gamma = matrix(-0.009, nrow=period, ncol=1),
-		ks_theta = matrix(-2.222, nrow=period, ncol=1),
-		ks_gamma = matrix(0.008, nrow=period, ncol=1),
-		ms_theta = matrix(-1.232, nrow=period, ncol=1),
-		ms_gamma = matrix(-0.009, nrow=period, ncol=1)
+		ls_theta = matrix(2.025, nrow=period, ncol=3),
+		ls_gamma = matrix(-0.009, nrow=period, ncol=3),
+		ks_theta = matrix(-2.222, nrow=period, ncol=3),
+		ks_gamma = matrix(0.008, nrow=period, ncol=3),
+		ms_theta = matrix(-1.232, nrow=period, ncol=3),
+		ms_gamma = matrix(-0.009, nrow=period, ncol=3)
 	)
 
   # rtss
@@ -228,6 +307,8 @@ for (batch_i in seq_along(batches)) {
     results <- lapply(batches[[batch_i]], process_row)
 
     outdata <- params[batches[[batch_i]],]
+    outdata$mosquito_params <- mosquito_params[batches[[batch_i]]]
+    outdata$mosquito_proportions <- apply(mosquito_proportions[batches[[batch_i]],,drop=F], 1, as.list)
     outdata$llin <- llins[batches[[batch_i]]]
     outdata$irs <- irs[batches[[batch_i]]]
     outdata$smc <- smc[batches[[batch_i]]]
@@ -238,7 +319,7 @@ for (batch_i in seq_along(batches)) {
     outdata$prev <- lapply(results, function(x) x$pv_0_36500)
     outdata$eir <- lapply(results, function(x) x$EIR)
 
-    jsonlite::write_json(outdata, outpath, pretty=TRUE)
+    jsonlite::write_json(outdata, outpath, auto_unbox=TRUE, pretty=TRUE)
     print(paste0('node ', node, ' batch ', batch_i, ' completed'))
     print(Sys.time())
     print(Sys.time() - start_time)
