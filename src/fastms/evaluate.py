0bd5b709f1eb8c7b79d6f3bece21531b6af7fa0d
Giovanni Charles <giovanni.charles@gmail.com>
2022-08-23 17:29:04 +0100

    Calibration statistics

--- src/fastms/evaluate.py
+++ src/fastms/evaluate.py
@@ -35,3 +35,28 @@ def test_prob_model(model, X_test, X_seq_test, y_test, y_scaler, n):
         y_scaler.inverse_transform(y_test).reshape(y_test.shape[0], -1)
     )
     logging.info(f'actual error: {actual_error}')
+
+    # calibration error
+    # space of CDF quantiles to observe
+    p = np.linspace(0.001, 1, endpoint=False)
+
+    # observed probabilities
+    dist = model((X_test, X_seq_test) * n)
+    F = dist.cdf(y_test).numpy()
+    p_hat = np.array([np.sum(F < pj) for pj in p]) / F.size
+    cal = np.sum(np.square(p - p_hat))
+
+    logging.info(f'calibration error: {cal}')
+
+    try:
+        shar = np.mean(dist.stddev())
+    except NotImplementedError:
+        try:
+            shar = np.mean(dist.stddev_approx())
+        except AttributeError:
+            logging.warn(
+                'Tensorflow probability needs to be upgraded to get sharpness info'
+            )
+            return
+
+    logging.info(f'sharpness: {shar}')
