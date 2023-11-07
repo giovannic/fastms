a35d0cb456a309af0cfd34270a48fb07a61449e3
Giovanni Charles <giovanni.charles@gmail.com>
2023-11-07 12:00:09 +0000

    Update rnn trainer to use RNNSurrogate

--- src/fastms/train/rnn.py
+++ src/fastms/train/rnn.py
@@ -1,23 +1,22 @@
 from jaxtyping import PyTree
-from flax.linen.module import _freeze_attr
 from jax import numpy as jnp
 from jax.tree_util import tree_map
 from mox.loss import mse
-from mox.seq2seq.rnn import make_rnn_surrogate
-from mox.seq2seq.training import train_seq2seq_surrogate
+from mox.seq2seq.rnn import RNNSurrogate, make_rnn_surrogate, init_surrogate
+from mox.seq2seq.training import train_rnn_surrogate
 
 def build(samples: PyTree):
-    (x, x_seq, x_t), y = _freeze_attr(samples)
+    (x, x_seq, x_t), y = samples
     n_steps = y['immunity'].shape[1]
     x_std = None
-    x_seq_std = _freeze_attr([
+    x_seq_std = [
         tree_map(lambda _: (0, 1), x_seq[0]), # interventions
         None # demography
-    ])
+    ]
     y_std = None
-    y_min = _freeze_attr(tree_map(lambda _: 0, y))
-    max_n = jnp.finfo(jnp.float32).max
-    y_max = _freeze_attr(tree_map(lambda _: max_n, y))
+    y_min = tree_map(lambda _: 0, y)
+    max_n = jnp.finfo(jnp.float64).max
+    y_max = tree_map(lambda _: max_n, y)
     return make_rnn_surrogate(
         x,
         x_seq,
@@ -31,13 +30,13 @@ def build(samples: PyTree):
         y_max
     )
 
-def init(model, samples, key):
-    (x, x_seq, _), _ = _freeze_attr(samples)
-    return model.init(key, (x, x_seq))
+def init(model: RNNSurrogate, samples, key):
+    (x, x_seq, _), _ = samples
+    return init_surrogate(key, model, (x, x_seq))
 
 def train(model, params, samples, key, epochs, n_batches):
-    (x, x_seq, _), y = _freeze_attr(samples)
-    params = train_seq2seq_surrogate(
+    (x, x_seq, _), y = samples
+    params = train_rnn_surrogate(
         (x, x_seq),
         y,
         model,
@@ -45,6 +44,6 @@ def train(model, params, samples, key, epochs, n_batches):
         mse,
         key,
         epochs = epochs,
-        n_batches = n_batches
+        batch_size = x[1].shape[0] // n_batches
     )
     return params
